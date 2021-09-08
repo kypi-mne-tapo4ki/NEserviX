6df0833832014e8d3b67cc222b8f232fb9578c43
kypi-mne-tapo4ki <d.l.ermolaev@yandex.ru>
2021-09-08 18:42:17 +0300

    refactoring pages

--- NEserviX/main/urls.py
+++ NEserviX/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('new_sale', views.new_sale),
 ]
--- NEserviX/main/views.py
+++ NEserviX/main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'main.html')
+    return render(request, 'main/main.html')
+
+
+def new_sale(request):
+    return render(request, 'main/new_sale.html')
+
